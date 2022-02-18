c2807ec8ddf19e565796825823f87a2d7d225cea
Bendik Waade <bendik@waade.net>
2022-02-18 14:44:54 +0100

    Added .from_expression factory and based scaling

--- objects/sequence.py
+++ objects/sequence.py
@@ -5,6 +5,7 @@ class Sequence(VGroup):
 
     def __init__(self, *points, **kwargs):
         super().__init__(**kwargs)
+        self.y_lock = 0
         self.add(*points)
     
     @staticmethod
@@ -16,17 +17,34 @@ class Sequence(VGroup):
         return Sequence(*points, **kwargs)
     
     @staticmethod
-    def from_expression(map, num=20, radius=.05):
+    def from_expression(map, num=20, radius=.05, **kwargs):
         '''map maps from naturals to sequence elements'''
-        pass
+        seq = map(np.arange(1, num+1))
+        positions = np.zeros((num, 3))
+        positions[:,0] = seq
+        points = [Dot(pos, radius=radius) for pos in positions]
+        return Sequence(*points, **kwargs)
+
+    def spread(self, factor, origin=0):
+        for dot in self:
+            dot.set_x((dot.get_x()-origin) * factor + origin)
+        return self
     
     def apply_function(self, func, **kwargs):
         '''Function is meant in the mathematical sense. Design methods
         are applied element-wise by default for VGroups'''
-        centers = self.get_centers()
-        shifts = func(centers[:,0], **kwargs)
-        for dot, sh in zip(self, shifts):
-            dot.shift(sh * UP)
+
+        for dot in self:
+            y = dot.get_y()
+            dot.set_y(y + func(dot.get_x(), **kwargs))
+        return self
+    
+    def lock_y(self):
+        self.y_lock = self.get_y()
+    
+    def collapse(self):
+        for dot in self:
+            dot.set_y(self.y_lock)
         return self
 
     
@@ -39,5 +57,5 @@ class Sequence(VGroup):
         
 
     def get_centers(self):
-        return np.array([point.get_arc_center() for point in self])
+        return np.array([point.get_center() for point in self])
 
--- scenes/testing.py
+++ scenes/testing.py
@@ -3,24 +3,19 @@ from objects.sequence import Sequence
 
 class TestScene(Scene):
     def construct(self):
-        seq = Sequence.from_range(-4, 4, num=400, radius=0.05)
-        seq.set_fill(RED)
-        f = lambda x: np.sin(2*x)
-        g = lambda x: .2*np.cos(10*x)
-        # along_f = seq.along_function(f)
-        # along_gf = along_f.along_function(g)
+        zoom = 5
+        fnc = lambda n: zoom - zoom/(np.sqrt(n))
+        seq = Sequence.from_expression(fnc, num=100, radius=0.02)
+        seq.set_fill(BLUE)
 
-        
-        #creations = [FadeIn(dot) for dot in seq]
         self.play(
             AnimationGroup(
                 *[FadeIn(dot) for dot in seq],
                 lag_ratio=.001
             )
         )
-        # Make animations before play
 
-        #self.play(*creations, run_time=1)
-        self.play(seq.animate.apply_function(f))
-        self.play(seq.animate.apply_function(g))
+        self.play(
+            seq.animate.spread(3, origin=fnc(100))
+        )
         self.wait()
