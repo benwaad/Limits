3ee5156130cb9b11f6f0b7960f90f1ed0f7465cc
Bendik Waade <bendik@waade.net>
2022-02-20 23:01:25 +0100

    Function and rate function figured out

--- scenes/testing.py
+++ scenes/testing.py
@@ -2,20 +2,19 @@ from manim import *
 from objects.sequence import Sequence
 
 class TestScene(Scene):
-    def pfunc(self, t):
-        return np.array([t, np.cos(3*t), 0])
     def construct(self):
+        def pfunc(t):
+            return np.array([t, np.cos(3*t), 0])
         graph = ParametricFunction(
-            self.pfunc,
+            pfunc,
             t_range=np.array([-5,5]),
             stroke_width=1
         )
         expr = lambda n: 2*PI/3 - 3/(n+1)
         seq = Sequence.on_graph(graph, expr, num=50, radius=.05).set_color(BLUE)
         
-        
-        self.add(graph)
-        self.play(Create(seq), run_time=1.2)
+        self.play(Create(graph))
+        self.play(ShowIncreasingSubsets(seq, rate_func=rate_functions.ease_in_quad), run_time=2)
         self.wait()
 
 
